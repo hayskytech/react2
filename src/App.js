c4fc1980f40f798a31cd025573af597a5949987e
sufyan <mdsufyan7@gmail.com>
2023-09-08 12:08:08 +0530

    news offset

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import 'semantic-ui-css/semantic.min.css'
 import React from 'react'
-import FetchNews from './comp/FetchNews'
+import NewsOffset from './comp/NewsOffset'
 
 export default function App() {
   return (
 
     <div>
-      <FetchNews />
+      <NewsOffset />
     </div>
 
   )
